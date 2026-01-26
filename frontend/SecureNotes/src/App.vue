0d9606da5461ce5466ad41c967f5f7dffc8540fa
Ayush <rasaileeayush@gmail.com>
2026-01-26 12:32:16 +0545

    main part where all teh components and pages are routed

---
+++ frontend/SecureNotes/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+// eslint-disable-next-line no-unused-vars
+import { RouterView, RouterLink } from 'vue-router'
+
+</script>
+
+<template>
+  <div>
+    <div >
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
